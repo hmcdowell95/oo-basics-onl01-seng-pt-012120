74e14968c78e3cf805a698c0ac18b474e0d8056a
Harley McDowell <hmcdowell95@gmail.com>
2020-02-06 21:51:29 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,9 @@
 class Shoe
-  attr_accessor :brand, :color, :size, :material, :condition
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
   
-  def initialize()
+  def initialize(brand)
+    @brand = brand
+  end
+ 
+  def 
